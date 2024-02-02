5a7732b6bda4ac9c3b4ab85134a6a16360ce5fa8
Aaron Perez <aperez0295@gmail.com>
2024-02-02 12:51:48 -0600

    Time: 41 ms (28.88%), Space: 16.5 MB (72.98%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -5,7 +5,7 @@ class Solution:
         result = []
         
         # Iterate over all possible string lengths: from the length of low to the length of high.
-        # Length is either 3
+        # Length in 1000 - 13000 is 4 or 5
         for length in range(len(str(low)), len(str(high)) + 1):
             for start in range(n - length):
                 # 0, 1, 2, 3, 4, 5, 6
