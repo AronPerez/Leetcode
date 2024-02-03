dc71b5ddf603e89144c7d3f725489a37f92b04ff
Aaron Perez <aperez0295@gmail.com>
2024-02-03 14:29:34 -0600

    Time: 33 ms (75.41%), Space: 16.6 MB (76.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,13 +3,12 @@ class Solution:
         if n == 0 or n == 1: # Can only reach these with 1 step
             return 1
         
-        dp = [0] * (n+1) # Index 0 -> index n + 1
-        dp[0] = dp[1] = 1 # Only 1 way to get to 0 and 1 (1 step)
-        
-        
-        for i in range(2, n+1):  
-            #0, 1, 2, 3, 4, 5 = [1, 1, 2, 3, 5, 8]
+        prev, curr = 1, 1 # Only 1 way to get to 0 and 1 (1 step)
+        for i in range(2, n+1):
+             #0, 1, 2, 3, 4, 5 = [1, 1, 2, 3, 5, 8]
             # Sum i-1 + i-2 values
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+            temp = curr
+            curr = prev + curr
+            prev = temp
+        return curr
         
