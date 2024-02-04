49723f5dec75cf60f375c1a73e80885b82f0ea15
Aaron Perez <aperez0295@gmail.com>
2024-02-04 14:57:12 -0600

    Time: 93 ms (76.44%), Space: 17.3 MB (64.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -9,7 +9,7 @@ class Solution:
         min_window = float('inf'), None, None  # Initialize minimum window
 
         while right < len(s):
-            c = s[right]
+            c = s[right] 
             if c in need:
                 need[c] -= 1
                 if need[c] >= 0:  # Only count characters that are still needed
@@ -17,14 +17,19 @@ class Solution:
 
             while missing == 0:  # Shrink window if possible
                 c = s[left]
+                # The character at the position pointed by the `left` pointer is no longer a part of the window.
                 if c in need:
                     need[c] += 1
                     if need[c] > 0:  # Stop shrinking if a character is no longer sufficient
                         missing += 1
-                if right - left + 1 < min_window[0]:
+                
+                if right - left + 1 < min_window[0]: # Save the smallest window until now.
                     min_window = (right - left + 1, left, right)
+                
+                # Move the left pointer ahead, this would help to look for a new window.                    
                 left += 1
-
+            
+            # Keep expanding the window once we are done contracting.
             right += 1
 
         return "" if min_window[0] == float('inf') else s[min_window[1]:min_window[2] + 1]
