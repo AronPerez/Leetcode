cd39e5a3ae8c0b725e9f0c045d108f111ec9f826
Aaron Perez <aperez0295@gmail.com>
2024-02-07 12:40:17 -0600

    Time: 324 ms (98.70%), Space: 31.8 MB (79.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,15 +4,14 @@ class Solution:
         longestStreak = 0
         
         for num in values:
+            # If it is the start of a sequence (no left neighbor means start of seq)
             if num - 1 not in values:
-                currentNum = num
-                currentStreak = 1
+                currentLength = 0
+                while num + currentLength in values:
+                    currentLength += 1
                 
-                while currentNum + 1 in values:
-                    currentNum += 1
-                    currentStreak += 1
-                    
-                longestStreak = max(longestStreak, currentStreak)
-            
+                longestStreak = max(longestStreak, currentLength)
+                
         return longestStreak
+                    
         
