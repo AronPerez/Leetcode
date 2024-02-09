fc366f1e725d728b419082e1d6f4fd0b2b8d0246
Aaron Perez <aperez0295@gmail.com>
2024-02-09 14:21:19 -0600

    Time: 69 ms (28.52%), Space: 16.9 MB (51.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,9 +3,9 @@ class Solution:
         ans = []
         
         for i, value in enumerate(nums1):
-            for j in nums2[nums2.index(value):]:
-                 if j > nums1[i]: # If we find a value greater than it, append and break
-                    ans.append(j)
+            for j in range(nums2.index(value), len(nums2)):
+                 if nums2[j] > nums1[i]: # If we find a value greater than it, append and break
+                    ans.append(nums2[j])
                     break
             else:
                 ans.append(-1)
