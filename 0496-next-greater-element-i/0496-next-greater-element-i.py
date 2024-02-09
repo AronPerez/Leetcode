578591f21e2490e5feaded0c6007f155fff55d8b
Aaron Perez <aperez0295@gmail.com>
2024-02-09 14:23:18 -0600

    Time: 43 ms (88.44%), Space: 17.1 MB (46.31%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,13 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
+        mapping = {}
+        result = []
+        stack = []
+        stack.append(nums2[0])
         
-        for i, value in enumerate(nums1):
-            for j in range(nums2.index(value), len(nums2)):
-                 if nums2[j] > nums1[i]: # If we find a value greater than it, append and break
-                    ans.append(nums2[j])
-                    break
-            else:
-                ans.append(-1)
         
-        return ans
+        for i in range(1, len(nums2)):
+            while stack and nums2[i] > stack[-1]:       # if stack is not empty, then compare it's last element with nums2[i]
+                mapping[stack[-1]] = nums2[i]           # if the new element is greater than stack's top element, then add this to dictionary 
+                stack.pop()                             # since we found a pair for the top element, remove it.
+            stack.append(nums2[i])                      # add the element nums2[i] to the stack because we need to find a number greater than this
+
+        for element in stack:                           # if there are elements in the stack for which we didn't find a greater number, map them to -1
+            mapping[element] = -1
+
+        for i in range(len(nums1)):
+            result.append(mapping[nums1[i]])
+
+        return result
