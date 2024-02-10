7a35a2a458a5d095084931957ba121df3928abdb
Aaron Perez <aperez0295@gmail.com>
2024-02-10 15:26:36 -0600

    Time: 31 ms (93.01%), Space: 16.6 MB (91.65%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -1,21 +1,19 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        # If not the same length, we cannot swap just 2 chars
-        if len(s) != len(goal):
+        if len(s) != len(goal): # If not the same length, we cannot swap just 2 chars
             return False
         
-        if s == goal:
+        if s == goal: # Identical strings or duplicates allow a trivial swap
             return not (len(set(s)) == len(s))
         
-        if set(s) != set(goal):
+        if set(s) != set(goal): # Disallowed if they don't have the same set of characters
             return False
         
-        
-        lst = []
+        mismatch_diffs = []  # Store offset of mismatched characters for swap validation
         for i in range(len(s)):
             if s[i] != goal[i]:
-                lst.append(ord(s[i]) - ord(goal[i]))
-                if len(lst) == 2:
-                    return s[i+1:] == goal[i+1:] if sum(lst) == 0 else False 
+                mismatch_diffs.append(ord(s[i]) - ord(goal[i]))
+                if len(mismatch_diffs) == 2: 
+                    return sum(mismatch_diffs) == 0 and s[i+1:] == goal[i+1:] 
         return False
         
