529da730273c2085e7395ca93ee120b7f9bd1c17
Aaron Perez <aperez0295@gmail.com>
2024-02-10 15:27:41 -0600

    Time: 42 ms (38.13%), Space: 16.6 MB (91.65%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -9,6 +9,15 @@ class Solution:
         if set(s) != set(goal): # Disallowed if they don't have the same set of characters
             return False
         
+        """
+        Difference Tracking: Creates a listmismatch_diffslst. The loop iterates through the strings comparing character by character. When differing characters are found
+            Calculate Offset: It calculates the difference between the ASCII (ord) 
+            values of the current mismatched characters (s[i], goal[i]) from the strings and appends it to lst.
+            Checking Swappability: Immediately when len(lst) becomes 2 (means we found two character differences), two things are checked:
+                Zero Sum: The elements in lst must sum to zero. This ensures the mismatches form a reverse pair for a correct swap, 
+                (e.g., 'x' swapped with 'y', then 'y' must be swapped with 'x' in the correct positions
+                Remaining Substring: The code tests if s[i+1:] == goal[i+1:], meaning the rest of the strings must exactly match after that position for a valid swap.
+        """
         mismatch_diffs = []  # Store offset of mismatched characters for swap validation
         for i in range(len(s)):
             if s[i] != goal[i]:
