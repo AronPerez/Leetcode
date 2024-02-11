eaf7b8785671c124fd3c4ab0dbcf25b69455ed50
Aaron Perez <aperez0295@gmail.com>
2024-02-11 11:50:20 -0600

    Time: 261 ms (51.89%), Space: 27.9 MB (12.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,21 +6,22 @@ class Solution:
         visited = set()
 
         def dfs(row: int, col: int) -> None:
-            # nonlocal visited
-            # check if we are out of bounds and we have not visited
+          # nonlocal visited
+          # check if we are out of bounds and we have not visited
             if (row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == "0" or (row, col) in visited):
+            # print((row, col) in visited)
                 return 
 
             # We need to know we have visited
             visited.add((row, col)) # {(0,0)}/ {(0,0), (0, 1))}/{(0,0), (0, 1)), (1,1)}/ {(0,0), (0, 1)), (1,1), (0,1)}
             # visited[row][col] = True
 
-            # left, right, up, down
+          # left, right, up, down
             dfs(row-1, col) # ob/in seen/1/ob
             dfs(row+1, col) # 1/is 0/inseen/0
             dfs(row, col+1) # ob/inseen/inSeen/ob
             dfs(row, col-1) # 1/0/0/inSeen
-            # if 0 <= row < rows and 0 <= col < cols and (row, col) not in visited:
+              # if 0 <= row < rows and 0 <= col < cols and (row, col) not in visited:
 
 
         for row in range(rows): # 0
@@ -29,5 +30,5 @@ class Solution:
                     dfs(row, col)
                     numOfIsland += 1 # 1
 
-
+ 
         return numOfIsland # 1
