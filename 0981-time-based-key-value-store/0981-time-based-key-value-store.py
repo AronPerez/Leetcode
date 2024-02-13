d3eb0f2da7f1d258789b8ff31934231b38471b6e
Aaron Perez <aperez0295@gmail.com>
2024-02-13 14:58:20 -0600

    Time: 594 ms (53.55%), Space: 74.3 MB (56.41%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,28 +1,31 @@
-from sortedcontainers import SortedDict
-
 class TimeMap:
-
     def __init__(self):
-        self.store = {}
+        self.store = defaultdict(list)
         
-
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = SortedDict()
-
-        self.store[key][timestamp] = value
+        self.store[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.store:
             return ""
         
-        iterator = self.store[key].bisect_right(timestamp)
-        if iterator == 0:
+        if timestamp < self.store[key][0][0]: # If the timestamp is less than first timestamped, no ans
             return ""
+        r = self.binarSearchHelper(key, timestamp)
+        return "" if r == 0 else self.store[key][r - 1][1]
+    
+    def binarSearchHelper(self, key: str, timestamp: int) -> int:
+        l, r = 0, len(self.store[key])
         
-        return self.store[key].peekitem(iterator - 1)[1]
+        while l < r:
+            m = l + (r - l) // 2
+            
+            if self.store[key][m][0] <= timestamp: # Value is likely on right
+                l = m + 1
+            else: # Value is likely on left
+                r = m
  
-                
+        return r
         
 
 
