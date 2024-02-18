2a391868a00b7281c83ccc6853d496e006de478d
Aaron Perez <aperez0295@gmail.com>
2024-02-18 15:53:00 -0600

    Time: 1370 ms (40.59%), Space: 24.4 MB (17.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        dp = [[False for _ in range(n)] for _ in range(n)]
+        dp = [[False] * n for _ in range(n)]
 
-        # Base case: all single-character substrings are palindromes
-        for i in range(n):
-            dp[i][i] = True
-
-        # Fill in the table in bottom-up manner
-        for l in range(2, n + 1):
-            for i in range(n - l + 1):
-                j = i + l - 1
-                if l == 2:
-                    dp[i][j] = (s[i] == s[j])
-                else:
-                    dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1]
-
-        # Find the longest palindrome substring
         start = 0
         max_len = 1
-        for i in range(n):
-            for j in range(i, n):
-                if dp[i][j] and j - i + 1 > max_len:
-                    start = i
-                    max_len = j - i + 1
+
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = True
+            for j in range(i + 1, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    if j - i + 1 > max_len:
+                        max_len = j - i + 1
+                        start = i
 
         return s[start:start + max_len]
