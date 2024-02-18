1ca386a7a9c854351ede011980fea87eab8b1e94
Aaron Perez <aperez0295@gmail.com>
2024-02-18 13:49:43 -0600

    Time: 3702 ms (22.38%), Space: 25 MB (13.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,33 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand(left: int, right: int) -> str:
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return s[left + 1:right]
-
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
+        dp = [[False for _ in range(n)] for _ in range(n)]
+
+        # Base case: all single-character substrings are palindromes
         for i in range(n):
             dp[i][i] = True
 
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                dp[i][i + 1] = True
+        # Fill in the table in bottom-up manner
+        for l in range(2, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
+                if l == 2:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1]
 
-        for length in range(3, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
-
-        max_len = 0
-        max_start = 0
+        # Find the longest palindrome substring
+        start = 0
+        max_len = 1
         for i in range(n):
             for j in range(i, n):
-                if dp[i][j] and max_len < j - i + 1:
+                if dp[i][j] and j - i + 1 > max_len:
+                    start = i
                     max_len = j - i + 1
-                    max_start = i
 
-        return s[max_start:max_start + max_len]
-        
+        return s[start:start + max_len]
