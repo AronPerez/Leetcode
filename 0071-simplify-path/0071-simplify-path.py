dcc9e491d65f1e24a5d059915d1a6d1aa2d7fc62
Aaron Perez <aperez0295@gmail.com>
2024-02-20 02:41:07 -0600

    Time: 27 ms (97.89%), Space: 16.7 MB (66.08%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -3,23 +3,15 @@ class Solution:
         # Delimit based on /
         # If it ends with a /, we can remove it since we dont care about it
         stack = []
-
-        
-        #W for value in stack:
-        
-        # Go through the stack and if you see a '', you know its a /
-        
-        # We can to reconstruct the ans using "".join(ans)
-        # ans[-1] cannot be a /, if it is we can remove it
-        for component in path.split('/'):
-            if component == '..':
+        components = path.split('/')
+        for component in components:
+            if component == '.' or component == '':
+                continue
+            elif component == '..':
                 if stack:
                     stack.pop()
-            elif component == '.' or not component:
-                continue
             else:
                 stack.append(component)
-
         return '/' + '/'.join(stack)
                 
                 
