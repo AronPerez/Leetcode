d792f5d0a83e31d28cd5a6c8a7d8fe9cd542efa3
Aaron Perez <aperez0295@gmail.com>
2024-02-20 20:13:13 -0600

    Time: 85 ms (18.93%), Space: 19.9 MB (66.61%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -11,17 +11,22 @@ class Solution:
         # if newInterval startTime less than interval i's endTime, there is overlap
         # We would need to make sure newInterval endTime is before i+1's startTime
         
-        res = []
-        
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-                
-        res.append(newInterval)
-        
-        return res
+        result = []
+        i = 0
+
+        # Add intervals that come before the new interval
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+
+        # Merge overlapping intervals 
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(intervals[i][0], newInterval[0]), 
+                            max(intervals[i][1], newInterval[1])]
+            i += 1
+        result.append(newInterval)  
+
+        # Add any remaining intervals
+        result += intervals[i:]
+
+        return result
