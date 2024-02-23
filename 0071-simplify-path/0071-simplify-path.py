9a03e4ec6900056606e7c7162bb7f64d61717eb7
Aaron Perez <aperez0295@gmail.com>
2024-02-23 12:43:35 -0600

    Time: 32 ms (87.50%), Space: 16.7 MB (63.30%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,22 +1,15 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # Delimit based on /
-        # If it ends with a /, we can remove it since we dont care about it
         stack = []
-        components = path.split('/')
-        for component in components:
-            if component == '.' or component == '':
-                continue
-            elif component == '..':
+        simplified_path = ""
+
+        for directory in path.split("/"):
+            if directory == "..":
                 if stack:
                     stack.pop()
-            else:
-                stack.append(component)
-        return '/' + '/'.join(stack)
-                
-                
-        
-                
-  
-            
-        
+            elif directory != "." and directory != "":
+                stack.append(directory)
+
+        simplified_path = "/" + "/".join(stack)
+
+        return simplified_path
