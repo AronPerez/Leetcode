bc0186ba17908207344be0c565a92f8e3a26f11c
Aaron Perez <aperez0295@gmail.com>
2024-02-26 13:14:46 -0600

    Time: 444 ms (73.77%), Space: 20.2 MB (58.43%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -2,19 +2,16 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         l = 0
         elementsToDelete = 1
-        # maxLen = 0
         for r in range(len(nums)):
             # If we included a zero in the window we reduce the value of k.
             # Since 1 max deletes we can do
             elementsToDelete -= 1 - nums[r]
             
-            
             if elementsToDelete < 0:
                 # We need to move the left element to throw out a zero
                 elementsToDelete += 1 - nums[l]
                 l += 1
-                
-                
+                   
         return r - l
                 
         
