e432dcdb26422cf893923ecad2afdedab22473b4
Aaron Perez <aperez0295@gmail.com>
2024-03-02 15:56:28 -0600

    Time: 3786 ms (36.51%), Space: 17.2 MB (55.35%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
@@ -1,5 +1,16 @@
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
+        """
+        Time: O(N^2*M^2)
+        
+        Each house, we will traverse all 0s. This will require O(# of 0's * # of 1's) times, so N*M
+        Only values we care about (1 and 0) are about half of the graph.
+        So their counts are O(M*N)/2 * O(M*N)/2 -> O(N^2*M^2))
+        
+        Space: O(N*M)
+        
+        Extra matrix used to track visited cells and store the sum and house counter for each empty cell
+        """
         # For each 1, start a BFS
             # When we final 0 cell, increase distance by + 1 based on steps already taken
         
