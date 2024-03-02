15e3e6d7e67857c1f80dad87c0b8e5ddb53f41e9
Aaron Perez <aperez0295@gmail.com>
2024-03-02 15:47:30 -0600

    Time: 3800 ms (36.09%), Space: 17.1 MB (62.85%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
@@ -1,50 +1,15 @@
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
-        # (2,2) 2-1 + 2 - 2 = 1
-        # (0,0) abs(0-1) + abs(0-2) = 3
+        # For each 1, start a BFS
+            # When we final 0 cell, increase distance by + 1 based on steps already taken
         
-        # Basically we need to pick the house thats closest to another house
-        # (0,0)/(2,2) -> ()
-        
-        # BFS from each empty cell
-        # Each next sell is 1 distancce further
-        
-        # Need a way to keep track of the num of houses we have reached from empty cell
-        """
-            Empty Land to Houses
-            
-            numOfHouses = 0
-            
-            for row in range(rows):
-                for col in range(cols):
-                    if grid[row][col] == 1: 
-                        numOfHouses += 1
-            minDistance = float('inf')
-            
-            for row in range(rows):
-                for col in range(cols):
-                    if grid[row][col] == 0: # Start BFS
-                    
-            return minDistance if minDistance != float('inf') else -1
-        """
-      
-        # Will need to assert that the empty cell BFS has reached n numOfHouses
-        # Once we reach a house, we take our distance and add it to a currentSum
-        # Basically that current sum will use min
-        # If we cannot reach all houses from empty cell, then that cell cannot be used
-            # We'd need to update all cells visited to 2, basically making it an obstacle so we do not waste time there
-        # And we would update a minDistance sum if we reached all houses
-        
-        numOfHouses = 0
+        numOfHouses = distances = 0
         rows, cols = len(grid), len(grid[0])
+        distances = [[[0,0] for _ in range(cols)] for _ in range(rows)]
         
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 1: 
-                    numOfHouses += 1
-       
         def bfs(row, col):
             nonlocal numOfHouses
+            nonlocal distances
             distanceSum = currentHouseCount = 0
             queue = deque([(row, col, 0)])
             visited = set()
@@ -53,10 +18,9 @@ class Solution:
                 row, col, step = queue.popleft()
                 
                 # Check if we reached a house 
-                if grid[row][col] == 1: # Is a house
-                    distanceSum += step
-                    currentHouseCount += 1
-                    continue
+                if grid[row][col] == 0: # Is a house
+                    distances[row][col][0] += step
+                    distances[row][col][1] += 1
                 
                 # Go left, right, up, down
                 for dR, dC in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
@@ -66,27 +30,24 @@ class Solution:
                     if 0 <= nR < rows and 0 <= nC < cols: # In bounds
                         # Ensure we have not visited before
                         # Also ensure not obstacle
-                        if (nR, nC) not in visited and grid[nR][nC] != 2:
+                        if (nR, nC) not in visited and grid[nR][nC] == 0:
                             visited.add((nR, nC))                            
                             queue.append((nR, nC, step+1))
-            
-            # We did not reach all houses
-            if currentHouseCount != numOfHouses: 
-                for row in range(rows):
-                    for col in range(cols):
-                        if grid[row][col] == 0 and (row, col) in visited: # Set as bound
-                            grid[row][col] = 2
-                            
-                return float('inf')
-            
-            # Reached all houses
-            return distanceSum
+        
+        
+                
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1: 
+                    numOfHouses += 1
+                    bfs(row, col)
                             
         minDistance = float('inf')
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 0:
-                    minDistance = min(minDistance,  bfs(row, col))
+                if distances[row][col][1] == numOfHouses:
+                    minDistance = min(minDistance,  distances[row][col][0])
+                     
                     
                     
         return minDistance if minDistance != float('inf') else -1
