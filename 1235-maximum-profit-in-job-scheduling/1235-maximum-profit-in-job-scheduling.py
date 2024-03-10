15070c2cd5d690c42b0245a65370cf303ab2dbe9
Aaron Perez <aperez0295@gmail.com>
2024-03-10 00:34:28 -0600

    Time: 706 ms (39.57%), Space: 28.8 MB (86.01%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -7,24 +7,11 @@ class Solution:
 
         dp = [0] * (n + 1)  # Create a DP array to store the maximum profit up to each job
 
-        def binarySearch(jobs, index):
-            left, right = 0, index - 1
-            while left <= right:
-                mid = left + (right - left) // 2
-                if jobs[mid][1] <= jobs[index][0]:
-                    if jobs[mid + 1][1] <= jobs[index][0]:
-                        left = mid + 1
-                    else:
-                        return mid
-                else:
-                    right = mid - 1
-            return -1
-
         for i in range(1, n + 1):  # Iterate through each job
             current_profit = jobs[i - 1][2]  # Get the profit of the current job
 
             # Find the index of the previous non-overlapping job using binary search
-            prev_job_index = binarySearch(jobs, i - 1)
+            prev_job_index = bisect.bisect_right(jobs, jobs[i - 1][0], key=lambda x: x[1]) - 1
 
             # Calculate the maximum profit by considering the current job's profit
             # and the maximum profit from the previous non-overlapping jobs
@@ -34,4 +21,3 @@ class Solution:
                 dp[i] = max(current_profit, dp[i - 1])
 
         return dp[n]  # Return the maximum profit
-        
