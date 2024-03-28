3f2baddb028332f7879022c3b01c7f9fb510461e
Aaron Perez <aperez0295@gmail.com>
2024-03-28 10:11:58 -0500

    Time: 540 ms (82.91%), Space: 77.5 MB (76.18%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,27 +1,25 @@
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = collections.OrderedDict()
         
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        else:
             return -1
-
-        self.cache.move_to_end(key)
-        return self.cache[key]
         
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.cache.move_to_end(key)
-        
-        self.cache[key] = value
-        
-        if len(self.cache) > self.capacity: # We remove LRU
-            # Continue to delete until at self.capacity size
-            self.cache.popitem(False)
+            self.cache[key] = value
+        else:
+            self.cache[key] = value
+            if len(self.cache) > self.capacity:
+                self.cache.popitem(False)
         
 
 
