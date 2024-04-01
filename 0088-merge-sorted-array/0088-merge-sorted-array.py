4bf26765d50f5237743897725f915c4283a0d9a7
Aaron Perez <aperez0295@gmail.com>
2024-04-01 18:25:23 -0500

    Time: 34 ms (88.75%), Space: 16.6 MB (78.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,8 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[i + m] = nums2[i]
+        last = m + n - 1
+        
+        # Merge in reverse
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]: # Go to last pos  and replace
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1
+            
+        # Fill for the remaining
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n, last = n - 1, last - 1
             
         
-        nums1.sort()
